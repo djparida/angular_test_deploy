9554cd917a73ee833dfb8038fd1c618b3f0b82ff
Dibyajyoti Parida <dibyajyoti@centroxy.com>
2020-10-12 12:05:09 +0530

    added lang change + UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','/src/styles.css','/src/argon.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+  constructor(private translate: TranslateService, public router: Router, private http: HttpClient){
+    translate.setDefaultLang('en');
+  }
+  title = 'clientApp';
+  data = true;
+  selectedLang: string = '';
+  public options: string[] = ["en", "fr","el"];
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
+  selectChangeHandler (event: any) {
+    this.selectedLang = event.target.value;
+    this.switchLanguage(this.selectedLang);
+  }
+  gluuLogout(){
+    let Router = this.router
+    let sessionID = sessionStorage.getItem("sessionID");
+    const headers = { 'Authorization': sessionID};
+    const body = {}
+    this.http.post<any>('http://localhost:8000/logout',body, { headers }).subscribe(data => {
+      console.log(data);
+      sessionStorage.clear();
+      Router.navigate(['authorize'])
+    })
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+const routes: Routes = [
+  // {path: 'authorize', component: AuthorizeComponent},
+  // {path: 'profile', component: ProfileComponent},
+  // {path: 'gluudata', component: GluuDataComponent},
+  // {path:  '', redirectTo:  '/authorize', pathMatch:  'full'},
+];
 
 @NgModule({
   declarations: [
@@ -10,9 +22,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
